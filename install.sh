825ac1c3086dc524696fd5aeba8c1b4030c6f448
Robine Savert <robine@hotmail.nl>
2020-05-07 09:49:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,25 +8,22 @@ if test ! $(which brew); then
 fi
 
 # Update Homebrew recipes
-brew update
+sudo brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
+sudo brew tap homebrew/bundle
+sudo brew bundle
 
 # Install PHP extensions with PECL
-pecl install memcached imagick
+sudo pecl install memcached imagick
 
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Projects
+sudo mkdir $HOME/Projects
 
 # Clone Github repositories
 ./clone.sh
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+sudo rm -rf $HOME/.zshrc
+sudo ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc

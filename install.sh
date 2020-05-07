4e2090f098ff46ea84f92141b50923e09f29baea
Robine Savert <robine@hotmail.nl>
2020-05-07 09:16:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,25 +14,15 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
 # Install PHP extensions with PECL
 pecl install memcached imagick
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer laravel/spark-installer laravel/valet
 
-# Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
-
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
-
-# Create sites subdirectories
-mkdir $HOME/Sites/blade-ui-kit
-mkdir $HOME/Sites/laravel
+mkdir $HOME/Projects
 
 # Clone Github repositories
 ./clone.sh
@@ -43,7 +33,3 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
-# Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos

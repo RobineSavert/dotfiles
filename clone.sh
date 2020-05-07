f0acbcd093580d038ed24d7758945ed45a67d30e
Robine Savert <robine@hotmail.nl>
2020-05-07 09:50:21 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,5 +5,5 @@ echo "Cloning repositories..."
 PROJECTS=$HOME/Projects
 
 # Simpel
-git clone git@gitlab.enrise.com:Simpel/SimpelBll.git $PROJECTS/simpelBLL
-git clone git@gitlab.enrise.com:Simpel/SimpelWeb.git $PROJECTS/simpelWeb
+sudo git clone git@gitlab.enrise.com:Simpel/SimpelBll.git $PROJECTS/simpelBLL
+sudo git clone git@gitlab.enrise.com:Simpel/SimpelWeb.git $PROJECTS/simpelWeb
